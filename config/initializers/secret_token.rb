3e6121b633ca926f5c93c48c2432250131781c8a
Eric <shaorui23@gmail.com>
2015-02-23 16:19:50 +0800

    Initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Receta::Application.config.secret_token = 'ec9accdf4d2c9f7e3ef841fc2aadc3b8952f58b68e63b5544ce05e7a7e0ee8b66feee4d104e8add34555ed90b7215459cb8cda2b80589a329a18125cb55764d7'
